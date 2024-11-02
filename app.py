47bee5079e04072a60b2cc175a17f10f6a7f339b
eleday <leotol1432@gmail.com>
2024-11-02 09:07:10 +0300

    phaser example init

--- app.py
+++ app.py
@@ -1,22 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import sys
 from io import StringIO
 
+
 app = Flask(__name__)
 
-@app.route('/execute', methods=['POST'])
-def execute_code():
-    code = request.json.get('code', '')
-    old_stdout = sys.stdout
-    redirected_output = sys.stdout = StringIO()
-    try:
-        exec(code)  # Выполнение кода
-        output = redirected_output.getvalue()
-        return jsonify({'output': output, 'error': None})
-    except Exception as e:
-        return jsonify({'output': None, 'error': str(e)})
-    finally:
-        sys.stdout = old_stdout
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
