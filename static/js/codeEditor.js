03ae2239bd469c77cb7de474dc6f02b9a6728f38
eleday <leotol1432@gmail.com>
2024-11-12 18:39:57 +0300

    added code-mirror

--- static/js/codeEditor.js
+++ static/js/codeEditor.js
@@ -35,3 +35,25 @@ btn.addEventListener('click', () => {
         }
     })();
 })
+
+const editor = CodeMirror.fromTextArea(document.getElementById('codeEditor'), {
+    lineNumbers: true,
+    mode: 'python',
+    theme: 'pycar-theme',
+    lineWrapping: true, 
+    tabSize: 4,
+    indentUnit: 4,
+    indentWithTabs: false,
+    smartIndent: true,
+    extraKeys: { "Ctrl-Space": "autocomplete" },
+    autoCloseBrackets: true,
+    hintOptions: {
+        completeSingle: false  // Отключаем автоматическое дополнение по первому совпадению
+    }
+});
+
+editor.on('inputRead', function(cm, change) {
+    if (change.text[0].match(/[a-zA-Z0-9_]/)) { // Если вводится буква, цифра или _
+        CodeMirror.commands.autocomplete(cm);
+    }
+});
