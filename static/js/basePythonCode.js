d0ad2baa838480ed3b3b2e66f264147d894232ef
eledays <leotol1432@gmail.com>
2024-12-01 21:29:19 +0300

    base user code eval on client side (no async)

---
+++ static/js/basePythonCode.js
@@ -0,0 +1,43 @@
+basePythonCode = `
+class Car:
+
+    def __init__(self):
+        self.power = 0
+        self.engine_started = False
+        self.wheel_angle = 0
+
+        self.error = None
+
+    def engine_start(self):
+        self.engine_started = True
+
+    def set_power(self, power: int):
+        if abs(power) <= 1:
+            self.power = power
+        else:
+            self.error = 'Мощность должна быть в диапазоне от -1 до 1'
+            
+    def rotate(self, deg):
+        if abs(deg) <= 30:
+            self.wheel_angle = deg * math.pi / 180
+        else:
+            self.error = 'Угол поворота должен быть в диапазоне от -30 до 30 градусов'
+
+    def to_dict(self):
+        r = {}
+
+        if self.error:
+            r = {'error': self.error}
+            self.error = None
+            return r
+
+        for name, value in vars(self).items():
+            # name = name.replace(f'_{self.__class__.__name__}__', '')
+            r[name] = value
+
+        return r
+
+
+car = Car()
+`;
+basePythonCode = `import sys`;
--- static/js/codeEditor.js
+++ static/js/codeEditor.js
@@ -1,5 +1,6 @@
 let btn = document.querySelector('#sendButton');
 let textarea = document.querySelector('#codeEditor');
+let output_block = document.querySelector('.code #output');
 
 const editor = CodeMirror.fromTextArea(document.getElementById('codeEditor'), {
     lineNumbers: true,
@@ -24,40 +25,36 @@ editor.on('inputRead', function(cm, change) {
 });
 
 btn.addEventListener('click', () => {
+    output_block.innerHTML = '';
     let code = editor.getValue();
-
-    console.log(JSON.stringify({ code: code }));
-
-    (async function() {
-        try {
-            const response = await fetch('/exec', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ 
-                    code: code,
-                })
-            })
-
-            if (response.ok) {
-                
-                const r = await response.json();
-                console.log(r);
-                if (r.error) {
-                    alert(r.error);
-                }
-                console.log(r);
-                window.mainCar = r.car;
-            }
-            else {
-                console.log('Ошибка при отправке запроса');
-            }
-        }
-        catch (error) {
-            console.error(error);
-        }
-    })();
+    evaluatePython(code);
 })
 
-editor.setValue('# Переменная car содержит экземпляр класса Car и относится к машине, расположенной слева\n\ncar.engine_start()  # запустить двигатель (с незапущенным не поедет)\ncar.set_power(.5)  # задать мощность двигателя\ncar.rotate(0)  # задать угол поворота колес в градусах (не более 30 по модулю)')
+editor.setValue('# Переменная car содержит экземпляр класса Car и относится к машине, расположенной слева\n\ncar.engine_start()  # запустить двигатель (с незапущенным не поедет)\ncar.set_power(.5)  # задать мощность двигателя\ncar.rotate(0)  # задать угол поворота колес в градусах (не более 30 по модулю)');
+
+async function load() {
+    let pyodide = await loadPyodide();
+    pyodide.setStdout({batched: (str) => output_block.innerHTML += '\n' + str})
+
+    console.log('ready');
+    document.querySelector('.loading_block').remove();
+
+    pyodide.runPython(basePythonCode);
+
+    return pyodide;
+};
+
+let pyodideReadyPromise = load();
+
+async function evaluatePython(code) {
+    let pyodide = await pyodideReadyPromise;
+    try {
+        let output = await pyodide.runPythonAsync(code);
+        console.log(output);
+        output_block.classList.remove('error');
+    } catch (err) {
+        console.log(err);
+        output_block.innerHTML = err;
+        output_block.classList.add('error');
+    }
+}
