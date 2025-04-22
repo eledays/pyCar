1429e4024bbc0d8662edd3d4be382f24ae038f0a
eledays <leotol1432@gmail.com>
2025-04-22 21:10:41 +0300

    Wiki added

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -49,7 +49,7 @@ addEventListener('mousemove', (event) => {
     cameraDeltaY = Math.min(cameraDeltaY, 3596);
 });
 
-helpButton.addEventListener('click', () => window.open('https://google.com', '_blank'));
+helpButton.addEventListener('click', () => window.open('https://github.com/eledays/pyCar/wiki/%D0%94%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D1%86%D0%B8%D1%8F-%D0%BF%D0%BE-%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D1%83-%D0%B0%D0%B2%D1%82%D0%BE%D0%BC%D0%BE%D0%B1%D0%B8%D0%BB%D1%8F', '_blank'));
 
 function resetWorld() {
     World.clear(world);
