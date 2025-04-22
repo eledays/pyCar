68f63f7820512005b33fbc27a61c70e0730c628f
eledays <leotol1432@gmail.com>
2025-04-22 21:30:38 +0300

    CodeExec.py - comments

--- app/CodeExec.py
+++ app/CodeExec.py
@@ -1,4 +1,4 @@
-# from app import app, socketio
+# Импорты библиотек
 from app.GameObjects import to_json
 
 import multiprocessing
@@ -11,25 +11,29 @@ import time
 from copy import deepcopy
 
 
+# Функция исполнения кода
 def execute_code(socketio, code, globals, locals={}):
-    def code_run(shared_dict, globals, locals):
+    # Функция исполнения кода, которая будет запущена в отдельном процессе для изоляции
+    def code_run(globals, locals):
         global do_check, output_capture, error
         try:
+            # Перенаправляем вывод кода в переменную
             output_capture = io.StringIO()
             sys.stdout = output_capture
 
+            # Исполняем код
             exec(code, globals, locals)
 
         except Exception as e:
+            # обрабатываем ошибку
             error = traceback.format_exc()
 
         finally:
             do_check = False
             sys.stdout = sys.__stdout__
 
-    shared_dict = dict()
-
-    code_run_thread = threading.Thread(target=code_run, args=(shared_dict, globals, locals))
+    # Создание и запуск потока для исполнения кода
+    code_run_thread = threading.Thread(target=code_run, args=(globals, locals))
     code_run_thread.start()
 
     global do_check, output_capture, error
@@ -39,12 +43,15 @@ def execute_code(socketio, code, globals, locals={}):
     error = None
     start_time = time.time()
 
+    # Пока исполняется код
     while do_check:
+        # Превышение максимально допустимого времени исполнения (2 минуты) - ошибка
         if time.time() - start_time >= 120:
             do_check = False
             sys.stdout = sys.__stdout__
             return {'error': 'Time error', 'game_objects': last_state}
 
+        # Если изменились игровые объекты, отправляем информацию об этом
         if last_state != to_json(globals['game_objects']):
             last_state = to_json(globals['game_objects'])
             socketio.emit('partial_result', {
